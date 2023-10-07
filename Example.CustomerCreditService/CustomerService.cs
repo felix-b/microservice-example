644f0dcabf65e7a288157b47096d98af89c1134d
bleno4ka@gmail.com <bleno4ka@gmail.com>
2023-10-07 19:26:57 +0300

    added service interface and class

---
+++ Example.CustomerCreditService/CustomerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CustomerService.BusinessLogic;
+
+public class CustomerService : ICustomerService
+{
+    public Task<GetCustomerCreditsResponse> GetCustomerCredits(GetCustomerCreditsRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IncrementCustomerCreditsResponse> IncrementCustomerCredits(IncrementCustomerCreditsRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Example.CustomerCreditService/ICustomerService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CustomerService.BusinessLogic;
+public interface ICustomerService
+{
+    Task<GetCustomerCreditsResponse> GetCustomerCredits(GetCustomerCreditsRequest request);
+    Task<IncrementCustomerCreditsResponse> IncrementCustomerCredits(IncrementCustomerCreditsRequest request);
+}
+
+public class IncrementCustomerCreditsRequest
+{
+    public int CustomerId { get; set; }
+    public double Credits { get; set; }
+}
+
+public class IncrementCustomerCreditsResponse
+{
+}
+
+public class GetCustomerCreditsRequest
+{
+    public int CustomerId { get; set; }
+}
+
+public class GetCustomerCreditsResponse
+{
+    public double Credits { get; set; }
+}
