dc6866e29098e309e181831a8c680179ad5a7f55
felix-b <felixberman76@gmail.com>
2023-10-08 02:29:01 +0300

    Add Swagger UI

--- Example.CustomerCreditService/Program.cs
+++ Example.CustomerCreditService/Program.cs
@@ -1,16 +1,25 @@
 using CustomerService.BusinessLogic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddSingleton<ICustomerService, CustomerService.BusinessLogic.CustomerService>();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapPost("/api.credits/customer/increment/{customerId}", async (ICustomerService service, int customerId, [FromBody]IncrementCreditsHttpBody body) => {
     var response = await service.IncrementCustomerCredits(new IncrementCustomerCreditsRequest {CustomerId = customerId, Credits = body.Amount });
     return new { 
         Message=$"OK"
     };
-});
+}).WithOpenApi();
 
 app.MapGet("/api.credits/customer/credits/{customerId}", async (ICustomerService service, int customerId) => {
     var response = await service.GetCustomerCredits(new GetCustomerCreditsRequest() {CustomerId=customerId });
@@ -19,7 +28,7 @@ app.MapGet("/api.credits/customer/credits/{customerId}", async (ICustomerService
         return Results.NotFound();
     }
     return Results.Json(response);
-});
+}).WithOpenApi();
 
 
 app.Run("http://localhost:3300");
